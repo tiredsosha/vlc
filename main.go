da23851e8c130b8b333ab93bcc1fd4d428dfbc48
Чичко Александра Алексеевна <aachichko@sberautotech.ru>
2022-07-28 17:19:48 +0300

    f

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	out2, err2 := exec.Command("bash", "-c", "pkill -f /bin/vlc").Output()
 	fmt.Println(out2)
 	fmt.Println(err2)
-	time.Sleep(20 * time.Second)
+	time.Sleep(3 * time.Second)
 	out, err := exec.Command("bash", "-c", "sudo systemctl restart vlc.service").Output()
 	fmt.Println(out)
 	fmt.Println(err)
